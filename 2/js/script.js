37e60ae9efe3788ead90bb4ce614a3bb27da979b
Gleb <glebsuper3@mail.ru>
2022-01-07 21:31:19 +0500

    new commit

---
+++ 2/js/script.js
@@ -0,0 +1,29 @@
+
+
+a = 15;
+console.log(a);
+
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+
+const obj = {
+    a: 50
+};
+
+obj.a = 10;
+
+console.log(obj);
+
+console.log(Name);
+var Name = 'Ivan';
+
+{
+    var result = 50;
+}
+
+console.log(result);
+
+alert(5);
+[].push('a');
---
+++ 6 ( Операторы )/js/script.js
@@ -0,0 +1,31 @@
+
+
+// console.log('arr' + ' - object');
+// console.log(4 + '6');
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2 + 2 * 2 != '6');
+
+// != Сравнения значений ( Не равно )
+// !== Сравнения типов данных ( Не равно )
+// == Сравнить Значения
+// === Сравнить Типы данных
+// && Логическое И
+// || Логическое Или
+
+
+const isChecked = false,
+      isClose = false;
+
+console.log(isChecked || !isClose);
+
