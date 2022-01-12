1b0dcc0ae267cd2cf88c6076090514e8259d5e8c
Gleb <glebsuper3@mail.ru>
2022-01-12 19:21:27 +0500

    interview questions

---
+++ 20/js/script.js
@@ -0,0 +1,36 @@
+"use strict";
+
+// let x = 5; alert( x++ );
+
+// [ ] + false - null + true
+// console.log([]+false-null+true); // [] - пустой массив получает строку
+                                    // null - если с ним проводить мат.операции то вернёт NaN
+
+// let y = 1;
+// let x = y = 2;
+// alert(x);
+
+// console.log([]+1+2);
+
+// alert("1"[0]);
+
+// console.log(2 && 1 && null && 0 && undefined);
+// И запинается на лжи
+
+// !!( a && b ) и (a && b)
+
+// console.log(!!( 1 && 2 ) === (1 && 2)); // !! - преваращает в boolean
+//             3     3   3
+// alert( null || 2 && 3 || 4 );
+
+// let a = [1, 2, 3]; 
+// let b = [1, 2, 3];
+// if (a==b){
+//     console.log('true');
+// }
+
+// alert(+"Infinity");
+
+// console.log("Ёжик">"яблоко");
+
+// console.log(0 || "" || 2 || undefined || true || false);
